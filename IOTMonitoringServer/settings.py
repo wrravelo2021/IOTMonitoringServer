f905b534e608d077b85903873c3395e3e94c6f22
William Ravelo M <drummerwilliam@gmail.com>
2022-09-15 15:33:35 -0500

    new ips

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "34.204.2.53"]
+ALLOWED_HOSTS = ["localhost", "44.204.40.74"]
 
 
 # Application definition
@@ -95,7 +95,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "18.234.162.106",  # Dirección IP de la base de datos
+        "HOST": "44.201.74.147",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -155,7 +155,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "3.82.96.220"
+MQTT_HOST = "52.87.202.170"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
